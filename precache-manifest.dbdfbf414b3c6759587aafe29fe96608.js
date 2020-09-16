f97f864e9028b5aae3218063bfa85bf339fad829
Xrenore3 <romanchaus519@gmail.com>
2020-09-16 23:21:49 +0300

    Updates

---
+++ precache-manifest.dbdfbf414b3c6759587aafe29fe96608.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e2115c2019b57a0a27ed595a93bc903",
+    "url": "/Xrenore3/ReactSocialNetwork/index.html"
+  },
+  {
+    "revision": "c5386f348d58c0c00aad",
+    "url": "/Xrenore3/ReactSocialNetwork/static/css/main.0f40e163.chunk.css"
+  },
+  {
+    "revision": "708b3afed08ce5d1324d",
+    "url": "/Xrenore3/ReactSocialNetwork/static/js/2.cd6c3976.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Xrenore3/ReactSocialNetwork/static/js/2.cd6c3976.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5386f348d58c0c00aad",
+    "url": "/Xrenore3/ReactSocialNetwork/static/js/main.2987d590.chunk.js"
+  },
+  {
+    "revision": "0a0c32ad76774ba01dc5",
+    "url": "/Xrenore3/ReactSocialNetwork/static/js/runtime-main.80147524.js"
+  },
+  {
+    "revision": "c06ccd001c0bee051862d13c43867491",
+    "url": "/Xrenore3/ReactSocialNetwork/static/media/logo.c06ccd00.svg"
+  },
+  {
+    "revision": "72f546576f4e290146261d0c043ab38f",
+    "url": "/Xrenore3/ReactSocialNetwork/static/media/preloader.72f54657.svg"
+  }
+]);
